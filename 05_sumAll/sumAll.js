cac9921ac8461744f16ad49bfd26f93189487530
AnthonyL7 <anthonylanda022@gmail.com>
2024-04-02 13:59:47 -0700

    Add numbers inbetween

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,12 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+  let sum = 0;
+  while (num1 <= num2) {
+    sum += num1;
+    num1++
+  }
+  return sum
 };
 
+sumAll(1, 4)
 // Do not edit below this line
 module.exports = sumAll;
