90f26e302f37e5fb9d63568bf653926e307545be
AnthonyL7 <anthonylanda022@gmail.com>
2024-12-20 16:01:33 -0800

    Refresher

---
+++ variables-operators/javascript.js
@@ -0,0 +1,54 @@
+console.log("Hello, World!");
+let name = 'John'
+let surname = 'Doe'
+
+console.log(name);
+console.log(surname);
+
+let age = 11;
+console.log(age);
+
+age = 54
+
+console.log(age);
+
+const pi = 3.14
+console.log(pi)
+
+//Assignment
+console.log(23+97);
+console.log(1+2+3+4+5+6);
+let express = (4+6+9)/77
+console.log(express)
+
+let a = 10
+console.log(a)
+console.log(9*a)
+let b = 7 * a
+console.log(b)
+
+const max = 57
+let actual = max - 13
+let percentage = actual / max
+console.log(percentage)
+
+let x = 2*2+1;
+console.log(x);
+
+a = 1;
+b = 2;
+
+let c = 3 - (a = b + 1);
+console.log(a);
+console.log(c);
+
+
+a = b = c = 2 + 2;
+console.log(a);
+console.log(b);
+console.log(c);
+
+
+let n = 2;
+n = n + 5;
+n = n * 2;
